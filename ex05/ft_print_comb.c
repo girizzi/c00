0e08993760373ae99b0f6c5cf86081bf1b2e1a8b
Giancarlo Rizzi <girizzi@e3r5p6.42roma.it>
2024-11-13 15:40:48 +0100

    ex00 to ex05

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -1,24 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: girizzi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/07 17:42:36 by girizzi           #+#    #+#             */
-/*   Updated: 2024/11/09 14:32:02 by girizzi          ###   ########.fr       */
+/*   Created: 2024/11/12 14:16:20 by girizzi           #+#    #+#             */
+/*   Updated: 2024/11/12 17:18:23 by girizzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_print_comb(void)
 {
-	write(1, &c, 1);
-}
+	int	a;
+	int	b;
+	int	c;
 
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				write(1, &a, 1);
+				write(1, &b, 1);
+				write(1, &c, 1);
+				if (!(a == '7' && b == '8' && c == '9'))
+					write(1, ", ", 2);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+/*
 int	main(void)
-
 {
-	ft_putchar('c');
+	ft_print_comb();
+	return (0);
 }
+*/
